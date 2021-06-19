11ec9e04efe5f93eae8de9a79b3ae688600f03e8
Evgeny Kuzmin <skchel174@gmail.com>
2021-06-19 16:30:40 +0500

    add temporary IndexController

---
+++ app/Controllers/IndexController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controllers;
+
+use Framework\Http\Client\Response\HtmlResponse;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+class IndexController
+{
+    public function index(ServerRequestInterface $request): ResponseInterface
+    {
+        $html = '<form action="/home" method="POST">
+                    <input type="text" name="input">
+                    <input type="submit">
+                </form>';
+        return new HtmlResponse($html);
+    }
+
+    public function page(ServerRequestInterface $request): ResponseInterface
+    {
+        return new HtmlResponse($request->getParsedBody()['input']);
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,33 +1,18 @@
 <?php
 
+use App\Controllers\IndexController;
 use Framework\Http\Client\Request\ServerRequestFactory;
 use Framework\Http\Router\RouteDispatcher;
 use Framework\Http\Router\RoutesCollection;
-use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\ResponseInterface;
 
 define('BASE_DIR', dirname(__DIR__));
 
 require_once BASE_DIR . '/vendor/autoload.php';
 
-class HomeController
-{
-    public function index(ServerRequestInterface $request)
-    {
-        echo '<form action="/home" method="POST">
-                <input type="submit">
-            </form>';
-    }
-
-    public function page(ServerRequestInterface $request)
-    {
-        echo __CLASS__ . ' / ' . __METHOD__ . '<br>';
-        var_dump($request->getAttributes());
-    }
-}
-
 $routes = new RoutesCollection();
-$routes->get('/', [HomeController::class, 'index'])->name('home');
-$routes->post('/{page}', [HomeController::class, 'page'])->params(['page' => '[a-z]+']);
+$routes->get('/', [IndexController::class, 'index'])->name('home');
+$routes->post('/{page}', [IndexController::class, 'page'])->params(['page' => '[a-z]+']);
 
 $router = new RouteDispatcher($routes);
 
@@ -39,6 +24,8 @@ foreach ($route->getAttributes() as $name => $value) {
 }
 
 [$handler, $method] = $route->getHandler();
-(new $handler)->$method($request);
+/** @var ResponseInterface $response */
+$response = (new $handler)->$method($request);
 
+echo $response->getBody();
 
