e9058d12103bad6f8b0cf1756ce1b19a568f3e85
Evgeny Kuzmin <skchel174@gmail.com>
2021-06-22 20:00:48 +0500

    update MiddlewareDispatcherTest method testAdd

--- tests/framework/Http/Middleware/MiddlewareDispatcher/MiddlewareDispatcherTest.php
+++ tests/framework/Http/Middleware/MiddlewareDispatcher/MiddlewareDispatcherTest.php
@@ -5,11 +5,11 @@ namespace Tests\framework\Http\Middleware\MiddlewareDispatcher;
 use Framework\Http\Middlewares\MiddlewareDispatcher\Interfaces\MiddlewareWrapperInterface;
 use Framework\Http\Middlewares\MiddlewareDispatcher\MiddlewareDispatcher;
 use Framework\Http\Middlewares\MiddlewareDispatcher\MiddlewareResolver;
-use Framework\Http\Middlewares\MiddlewareDispatcher\Next;
 use PHPUnit\Framework\MockObject\MockObject;
 use PHPUnit\Framework\TestCase;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
 class MiddlewareDispatcherTest extends TestCase
@@ -19,19 +19,20 @@ class MiddlewareDispatcherTest extends TestCase
 
     public function setUp(): void
     {
-        $this->resolver = $this
-            ->getMockBuilder(MiddlewareResolver::class)
-            ->getMock();
+        $this->resolver = $this->getMockBuilder(MiddlewareResolver::class)->getMock();
         $this->dispatcher = new MiddlewareDispatcher($this->resolver);
     }
 
     public function testAdd(): void
     {
+        $middleware = $this->getMockBuilder(MiddlewareInterface::class)->getMock();
+
         $this->resolver
             ->expects($this->exactly(1))
-            ->method('resolve');
+            ->method('resolve')
+            ->with($this->identicalTo($middleware));
 
-        $result = $this->dispatcher->add(function () {});
+        $result = $this->dispatcher->add($middleware);
 
         $this->assertInstanceOf(MiddlewareWrapperInterface::class, $result);
     }
