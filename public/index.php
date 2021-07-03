4278333ddb8168a0383265e63c00d5f2c1dff1f2
Evgeny Kuzmin <skchel174@gmail.com>
2021-07-03 19:40:59 +0500

    move middlewares from index to file

--- public/index.php
+++ public/index.php
@@ -1,15 +1,10 @@
 <?php
 
-use App\Http\Controllers\IndexController;
-use App\Http\Middlewares\AppPerformanceMiddleware;
-use Framework\Container\Container;
+use Framework\Container\Interfaces\ContainerInterface;
 use Framework\Http\Client\Request\ServerRequestFactory;
+use Framework\Http\Middlewares\MiddlewareDispatcher\Interfaces\MiddlewareDispatcherInterface;
 use Framework\Http\Middlewares\RequestHandler\RequestHandler;
-use Framework\Http\Middlewares\MiddlewareDispatcher\MiddlewareDispatcher;
-use Framework\Http\Middlewares\MiddlewareDispatcher\MiddlewareResolver;
-use Framework\Http\Middlewares\RequestHandler\RequestHandlerResolver;
-use Framework\Http\Middlewares\RouteDispatchMiddleware;
-use Framework\Http\Router\RouteDispatcher;
+
 
 define('BASE_DIR', dirname(__DIR__));
 define('START_TIME', microtime(true));
@@ -18,20 +13,15 @@ define('START_MEMORY', memory_get_usage());
 require_once BASE_DIR . '/vendor/autoload.php';
 
 require BASE_DIR . '/setup/provider.php';
-
-/** @var Container $container */
-$router = $container->get(RouteDispatcher::class);
+require BASE_DIR . '/setup/middlewares.php';
 
 $request = (new ServerRequestFactory)->createFromSapi();
 
-$middlewareDispatcher = new MiddlewareDispatcher(new MiddlewareResolver());
-
-$middlewareDispatcher->add(new RouteDispatchMiddleware($router));
-$middlewareDispatcher
-    ->add(AppPerformanceMiddleware::class)
-    ->route('home');
-
-$response = $middlewareDispatcher->process($request, new RequestHandler(new RequestHandlerResolver()));
+/**
+ * @var MiddlewareDispatcherInterface $middlewareDispatcher
+ * @var ContainerInterface $container
+ */
+$response = $middlewareDispatcher->process($request, $container->get(RequestHandler::class));
 
 header(sprintf('HTTP/%s %d %s',
     $response->getProtocolVersion(),
