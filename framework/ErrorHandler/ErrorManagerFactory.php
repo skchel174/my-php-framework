2d0ba16d736431d18ea31a2a1935a1c24c395277
Evgeny Kuzmin <skchel174@gmail.com>
2021-07-25 22:35:03 +0500

    replaced ErrorHandlersFactory by ErrorManagerFactory

---
+++ framework/ErrorHandler/ErrorManagerFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework\ErrorHandler;
+
+use Framework\ErrorHandler\Interfaces\ErrorManagerInterface;
+use Psr\Container\ContainerInterface;
+
+class ErrorManagerFactory
+{
+    const HANDLERS_FILE = BASE_DIR . '/setup/error-handlers.php';
+
+    public function __invoke(ContainerInterface $container): ErrorManagerInterface
+    {
+        $collection = $container->get(HandlersCollection::class);
+        $this->handlers($collection);
+        return new ErrorManager($collection, $container->get('config.debug'));
+    }
+
+    protected function handlers(HandlersCollection $collection): void
+    {
+        require static::HANDLERS_FILE;
+    }
+}
