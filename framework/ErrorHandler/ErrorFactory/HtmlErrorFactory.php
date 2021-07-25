5a1bd1b2c8ef9562756128b02e455213179cb666
Evgeny Kuzmin <skchel174@gmail.com>
2021-07-25 22:44:04 +0500

    remove debug parameter from ErrorFactories

--- framework/ErrorHandler/ErrorFactory/HtmlErrorFactory.php
+++ framework/ErrorHandler/ErrorFactory/HtmlErrorFactory.php
@@ -11,22 +11,16 @@ use Whoops\Run;
 class HtmlErrorFactory extends ErrorFactory
 {
     private RendererInterface $renderer;
-    private bool $debug;
     private array $templates;
 
-    public function __construct(RendererInterface $renderer, bool $debug, array $templates)
+    public function __construct(RendererInterface $renderer, array $templates)
     {
         $this->renderer = $renderer;
-        $this->debug = $debug;
         $this->templates = $templates;
     }
 
     public function create(\Exception $e): ResponseInterface
     {
-        if ($this->debug) {
-            throw $e;
-        }
-
         $code = $this->normalizeCode($e->getCode());
         $template = $this->templates[$code] ?? $this->templates['default'];
         $html = $this->renderer->render($template, [
--- framework/ErrorHandler/ErrorFactory/JsonErrorFactory.php
+++ framework/ErrorHandler/ErrorFactory/JsonErrorFactory.php
@@ -9,19 +9,8 @@ use Whoops\Run;
 
 class JsonErrorFactory extends ErrorFactory
 {
-    private bool $debug;
-
-    public function __construct(bool $debug)
-    {
-        $this->debug = $debug;
-    }
-
     public function create(\Exception $e): ResponseInterface
     {
-        if ($this->debug) {
-            throw $e;
-        }
-
         $code = $this->normalizeCode($e->getCode());
         return new JsonResponse(['code' => $code, 'message' => $e->getMessage()], $code);
     }
