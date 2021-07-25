85d4a1bd06756260d12b36ce39b29de7dc702a45
Evgeny Kuzmin <skchel174@gmail.com>
2021-07-25 22:42:09 +0500

    ContentTypeHelper

---
+++ framework/Helpers/ContentTypeHelper.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Framework\Helpers;
+
+use Psr\Http\Message\ServerRequestInterface;
+
+trait ContentTypeHelper
+{
+    public function getResponseType(ServerRequestInterface $request): string
+    {
+        $header = $request->getHeaderLine('Accept') ?? $request->getHeaderLine('Content-Type');
+
+        if (str_contains($header, 'json')) {
+            return 'json';
+        }
+
+        if (str_contains($header, 'xml')) {
+            return 'xml';
+        }
+
+        return 'html';
+    }
+}
