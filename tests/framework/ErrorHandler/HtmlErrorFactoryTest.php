cf74bad18fc10367a3160aa0a273c1d24aad7977
Evgeny Kuzmin <skchel174@gmail.com>
2021-07-25 23:07:00 +0500

    update error handle tests

--- tests/framework/ErrorHandler/HtmlErrorFactoryTest.php
+++ tests/framework/ErrorHandler/HtmlErrorFactoryTest.php
@@ -11,35 +11,16 @@ use Whoops\Run;
 
 class HtmlErrorFactoryTest extends TestCase
 {
-    public function testDebugCreate()
+    public function testCreate(): void
     {
-        $config = ['debug' => true];
-        $renderer = $this->getMockBuilder(RendererInterface::class)->getMock();
-        $exception = new \Exception('Exception message', 404);
-
-        $factory = new HtmlErrorFactory($renderer, new Run(), $config);
-        $response = $factory->create($exception);
-
-        $this->assertInstanceOf(ResponseInterface::class, $response);
-        $this->assertInstanceOf(HtmlResponse::class, $response);
-
-        $this->assertEquals($response->getStatusCode(), $exception->getCode());
-        $this->assertStringContainsString($response->getBody(), $exception->getMessage());
-    }
-
-    public function testProductionCreate(): void
-    {
-        $config = [
-            'debug' => false,
-            'error' => ['templates' => ['404' => '404.phtml']],
-        ];
+        $config = ['404' => '404.phtml'];
         $renderer = $this->getMockBuilder(RendererInterface::class)->getMock();
         $renderer->expects($this->once())
             ->method('render')
-            ->withConsecutive([$config['error']['templates']['404']])
+            ->withConsecutive([$config['404']])
             ->willReturn($message = 'Exception message');
 
-        $factory = new HtmlErrorFactory($renderer, new Run(), $config);
+        $factory = new HtmlErrorFactory($renderer, $config);
         $response = $factory->create($exception = new \Exception($message, 404));
 
         $this->assertInstanceOf(ResponseInterface::class, $response);
