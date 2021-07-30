1a2c89612ff46e35395fd7faac4f8968bd8789b5
skchel174 <skchel174@gmail.com>
2021-07-30 16:26:09 +0500

    add SessionStartMiddleware

---
+++ framework/Http/Middlewares/SessionStartMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Framework\Http\Middlewares;
+
+use Framework\Http\Sessions\Interfaces\SessionInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+class SessionStartMiddleware implements MiddlewareInterface
+{
+    private SessionInterface $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        $this->session->start();
+        return $handler->handle($request);
+    }
+}
