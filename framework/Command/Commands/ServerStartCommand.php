957ed7b8269351df9bd345496e1e0ed61c5436e7
skchel174 <skchel174@gmail.com>
2021-08-03 15:05:45 +0500

    add commands

---
+++ framework/Command/Commands/ServerStartCommand.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Framework\Command\Commands;
+
+use Framework\Command\Command;
+use Framework\Command\Input;
+use Framework\Command\Output;
+
+class ServerStartCommand extends Command
+{
+    protected function name(): string
+    {
+        return 'serve';
+    }
+
+    protected function description(): string
+    {
+        return 'starting development local server';
+    }
+
+    protected function options(): array
+    {
+        return [
+            'port' => 'port to start local server',
+        ];
+    }
+
+    public function execute(Input $input): Output
+    {
+        $port = $input->getOption('port') ?? 8000;
+        exec('php -S 127.0.0.1:' . $port . ' -t public');
+        return new Output('');
+    }
+}
